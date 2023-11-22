faa1b09220592bab1414663fa978f0845b38b76a
Jaime Salinas <jaime.salinas@supergirosatlantico.co>
2023-11-22 15:48:55 -0500

    cambios en home

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
       <h2>Bienvenidos A Una Lista De Tareas</h2>
       <p>Una lista de tareas es, simplemente, una lista de <br></br>cosas que hacer. En ella se apunta, punto</p>
       <p> por punto, todas aquellas tareas<br></br> (ya sean de nuestro trabajo o de nuestra vida privada) de </p>
-      <h2>las que tenemos que hacernos cargo.<br></br> Esta lista puede organizarse para un solo día, para una </h2>
-      <h2>semana, o bien en forma de proyecto.</h2>
+      <h2> Esta lista puede organizarse para un solo día, para una semana</h2>
+      <h2> bien en forma de proyecto.</h2>
     </div>
   );
 };
